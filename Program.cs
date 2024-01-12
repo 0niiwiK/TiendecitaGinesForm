34a8e4bbbe6d2b2404791caa040b1aea7df1fdca
0niiwiK <115632110+0niiwiK@users.noreply.github.com>
2024-01-12 22:57:13 +0100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,7 +1,10 @@
 namespace TiendecitaGines
 {
+    using System.Collections.Generic;
+    using System.IO;
     using System.Runtime.Serialization.Formatters.Binary;
-
+    using System.Text;
+    
     internal static class Program
     {
         public static List<Bocadillo> lista_bocadillos { get; private set; }
@@ -35,25 +38,69 @@ namespace TiendecitaGines
         }
 
         public static List<Bocadillo> CargarBocadillos(string ruta)
+    {
+        List<Bocadillo> bocadillos = new List<Bocadillo>();
+
+        using (FileStream fs = new FileStream(ruta, FileMode.Open))
+        using (BinaryReader reader = new BinaryReader(fs))
         {
-            List<Bocadillo> bocadillos = new List<Bocadillo>();
-            BinaryFormatter bf = new BinaryFormatter();
-            FileStream fs = new FileStream(ruta, FileMode.Open);
-            bocadillos = (List<Bocadillo>)bf.Deserialize(fs);
-            fs.Close();
-            return bocadillos;
+            int count = reader.ReadInt32(); // Lee la cantidad de bocadillos
+
+            for (int i = 0; i < count; i++)
+            {
+                string nombre = LeerStringConTamanio(reader);
+                int stock = reader.ReadInt32();
+                double precio = reader.ReadDouble();
+                char tipoPan = reader.ReadChar();
+                bool calentito = reader.ReadBoolean();
+
+                int imagenSize = reader.ReadInt32();
+                byte[] imagen = reader.ReadBytes(imagenSize);
+
+                Bocadillo bocadillo = new Bocadillo(nombre, stock, precio, tipoPan, calentito, imagen);
+                bocadillos.Add(bocadillo);
+            }
         }
 
-        public static void GuardarBocadillos(List<Bocadillo> lista, string ruta)
-        {
-            BinaryFormatter bf = new BinaryFormatter();
-            FileStream fs = new FileStream(ruta, FileMode.Create);
-            bf.Serialize(fs, lista);
-            fs.Flush();
-            fs.Close();
-        }
+        return bocadillos;
+    }
 
-        [Serializable]
+         public static void GuardarBocadillos(List<Bocadillo> lista, string ruta)
+    {
+        using (FileStream fs = new FileStream(ruta, FileMode.Create))
+        using (BinaryWriter writer = new BinaryWriter(fs))
+        {
+            writer.Write(lista.Count); // Escribe la cantidad de bocadillos
+
+            foreach (var bocadillo in lista)
+            {
+                EscribirStringConTamanio(writer, bocadillo.Nombre);
+                writer.Write(bocadillo.Stock);
+                writer.Write(bocadillo.Precio);
+                writer.Write(bocadillo.TipoPan);
+                writer.Write(bocadillo.Calentito);
+
+                writer.Write(bocadillo.Imagen.Length);
+                writer.Write(bocadillo.Imagen);
+            }
+        }
+    }
+
+         private static string LeerStringConTamanio(BinaryReader reader)
+    {
+        int length = reader.ReadInt32();
+        byte[] stringBytes = reader.ReadBytes(length);
+        return Encoding.UTF8.GetString(stringBytes);
+    }
+
+    private static void EscribirStringConTamanio(BinaryWriter writer, string value)
+    {
+        byte[] stringBytes = Encoding.UTF8.GetBytes(value);
+        writer.Write(stringBytes.Length);
+        writer.Write(stringBytes);
+    }
+}
+
         public class Bocadillo
         {
             public string Nombre { get; set; }
