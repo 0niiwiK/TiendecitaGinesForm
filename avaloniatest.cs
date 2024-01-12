55fc2494f3adf9f3bae53612a359025696953fc4
0niiwiK <115632110+0niiwiK@users.noreply.github.com>
2024-01-12 23:10:34 +0100

    Update avaloniatest.cs

--- avaloniatest.cs
+++ avaloniatest.cs
@@ -141,43 +141,207 @@ public class MainWindow : Window
     }
 
     // Add your event handlers here
-    private void btnNew_Click(object sender, RoutedEventArgs e)
+   private void btnNew_Click(object sender, RoutedEventArgs e)
+{
+    limpiarTextos();
+    ActivarTextos();
+    btnSaveBocadillo.Enabled = true;
+    btnSaveBocadillo.Visible = true;
+    btnAddImage.Visible = true;
+    btnAddImage.Enabled = true;
+    btnImage.Enabled = false;
+    btnImage.Visible = true;
+    pbImage.Source = null;
+    cbCalentar.Visible = false;
+    cbCalentito.Visible = true;
+    cbCalentito.IsChecked = false;
+}
+
+private void btnDelete_Click(object sender, RoutedEventArgs e)
+{
+    if (lista_bocadillos.Count > 0)
     {
-        // Implement your logic for the "Nuevo" button click
+        lista_bocadillos.Remove(bocadillo_actual);
+        if (num_boc_act >= lista_bocadillos.Count)
+            num_boc_act--;
+
+        txtNumBocadillos.Text = (num_boc_act + 1) + "/" + lista_bocadillos.Count;
+        MostrarSiHay();
+    }
+}
+
+private void btnImage_Click(object sender, RoutedEventArgs e)
+{
+    openFileDialog1.InitialDirectory = carpetaIncial;
+    openFileDialog1.Filter = "Image files (*.png;*.bmp;*.jpg;*.jpeg)|*.png;*.bmp;*.jpg;*.jpeg|All files (*.*)|*.*";
+    openFileDialog1.FilterIndex = 1;
+    openFileDialog1.RestoreDirectory = true;
+    openFileDialog1.FileName = "";
+    openFileDialog1.ShowDialog(this);
+}
+
+private void btnPrev_Click(object sender, RoutedEventArgs e)
+{
+    if (num_boc_act > 0)
+    {
+        num_boc_act--;
+        CargarBocadillo(num_boc_act);
+    }
+}
+
+private void btnNext_Click(object sender, RoutedEventArgs e)
+{
+    if (num_boc_act < lista_bocadillos.Count - 1)
+    {
+        num_boc_act++;
+        CargarBocadillo(num_boc_act);
+    }
+}
+
+private void btnSave_Click(object sender, RoutedEventArgs e)
+{
+    Program.GuardarBocadillos(lista_bocadillos, ruta);
+}
+
+private void btnSaveBocadillo_Click(object sender, RoutedEventArgs e)
+{
+    char tipopan;
+    switch (combTipoPan.SelectedIndex)
+    {
+        case 0:
+            tipopan = 'A'; break;
+        case 1:
+            tipopan = 'G'; break;
+        case 2:
+            tipopan = 'V'; break;
+        default:
+            tipopan = 'A'; break;
     }
 
-    private void btnDelete_Click(object sender, RoutedEventArgs e)
+    try
     {
-        // Implement your logic for the "Eliminar" button click
+        Program.Bocadillo nuevo_boc = new Program.Bocadillo(txtProducto.Text, (int)txtStock.Value, double.Parse(txtPrecio.Text), tipopan, cbCalentito.IsChecked ?? false);
+        lista_bocadillos.Add(nuevo_boc);
+
+        if (imagenActual != null)
+        {
+            nuevo_boc.Imagen = new byte[imagenActual.Length];
+            imagenActual.CopyTo(nuevo_boc.Imagen, 0);
+        }
+
+        num_boc_act = lista_bocadillos.Count - 1;
+    }
+    catch (Exception ex)
+    {
+        // Handle exception, e.g., show an error message
+    }
+    finally
+    {
+        MostrarSiHay();
+    }
+}
+
+private void btnAddImage_Click(object sender, RoutedEventArgs e)
+{
+    openAddImage.InitialDirectory = carpetaIncial;
+    openAddImage.Filter = "Image files (*.png;*.bmp;*.jpg;*.jpeg)|*.png;*.bmp;*.jpg;*.jpeg|All files (*.*)|*.*";
+    openAddImage.FilterIndex = 1;
+    openAddImage.RestoreDirectory = true;
+    openAddImage.FileName = "";
+    openAddImage.ShowDialog(this);
+}
+
+private void btnImage_Click_WinForms(object sender, RoutedEventArgs e)
+    {
+        openFileDialog1.InitialDirectory = carpetaIncial;
+        openFileDialog1.Filter = "Image files (*.png;*.bmp;*.jpg;*.jpeg)|*.png;*.bmp;*.jpg;*.jpeg|All files (*.*)|*.*";
+        openFileDialog1.FilterIndex = 1;
+        openFileDialog1.RestoreDirectory = true;
+        openFileDialog1.FileName = "";
+        openFileDialog1.ShowDialog(this); // Pass the current window instance as the owner
     }
 
-    private void btnImage_Click(object sender, RoutedEventArgs e)
+    private void btnAddImage_Click_WinForms(object sender, RoutedEventArgs e)
     {
-        // Implement your logic for the "Cambiar Imagen" button click
+        openAddImage.InitialDirectory = carpetaIncial;
+        openAddImage.Filter = "Image files (*.png;*.bmp;*.jpg;*.jpeg)|*.png;*.bmp;*.jpg;*.jpeg|All files (*.*)|*.*";
+        openAddImage.FilterIndex = 1;
+        openAddImage.RestoreDirectory = true;
+        openAddImage.FileName = "";
+        openAddImage.ShowDialog(this); // Pass the current window instance as the owner
     }
 
-    private void btnPrev_Click(object sender, RoutedEventArgs e)
+    private void btnSaveBocadillo_Click_WinForms(object sender, RoutedEventArgs e)
     {
-        // Implement your logic for the "⬅ Anterior" button click
+        char tipopan;
+        switch (combTipoPan.SelectedIndex)
+        {
+            case 0:
+                tipopan = 'A'; break;
+            case 1:
+                tipopan = 'G'; break;
+            case 2:
+                tipopan = 'V'; break;
+            default:
+                tipopan = 'A'; break;
+        }
+        try
+        {
+            Program.Bocadillo nuevo_boc = new Program.Bocadillo(txtProducto.Text, (int)txtStock.Value, double.Parse(txtPrecio.Text), tipopan, cbCalentito.Checked);
+            lista_bocadillos.Add(nuevo_boc);
+            if (imagenActual != null)
+            {
+                nuevo_boc.Imagen = new byte[imagenActual.Length];
+                imagenActual.CopyTo(nuevo_boc.Imagen, 0);
+            }
+            num_boc_act = lista_bocadillos.Count - 1;
+        }
+        catch (Exception ex)
+        {
+
+        }
+        finally { MostrarSiHay(); }
     }
 
-    private void btnNext_Click(object sender, RoutedEventArgs e)
+    private void openFileDialog1_FileOk(object sender, System.ComponentModel.CancelEventArgs e)
     {
-        // Implement your logic for the "Siguiente ⮕" button click
+        //Get the path of specified file
+        string filePath = openFileDialog1.FileName;
+        carpetaIncial = openFileDialog1.FileName;
+
+        //Read the contents of the file into a stream
+        var fileStream = openFileDialog1.OpenFile();
+
+        if (fileStream != null)
+        {
+            bocadillo_actual.Imagen = new byte[fileStream.Length];
+            using (MemoryStream ms = new MemoryStream())
+            {
+                fileStream.CopyTo(ms);
+                bocadillo_actual.Imagen = ms.ToArray();
+                pbImage.Source = new Bitmap(ms);
+            }
+        }
     }
 
-    private void btnSave_Click(object sender, RoutedEventArgs e)
+    private void openAddImage_FileOk(object sender, System.ComponentModel.CancelEventArgs e)
     {
-        // Implement your logic for the "GUARDAR ARCHIVO" button click
-    }
+        //Get the path of specified file
+        string filePath = openAddImage.FileName;
+        carpetaIncial = openAddImage.FileName;
 
-    private void btnSaveBocadillo_Click(object sender, RoutedEventArgs e)
-    {
-        // Implement your logic for the "Guardar" button click
-    }
+        //Read the contents of the file into a stream
+        var fileStream = openAddImage.OpenFile();
 
-    private void btnAddImage_Click(object sender, RoutedEventArgs e)
-    {
-        // Implement your logic for the "Agregar Imagen" button click
+        if (fileStream != null)
+        {
+            imagenActual = new byte[fileStream.Length];
+            using (MemoryStream ms = new MemoryStream())
+            {
+                fileStream.CopyTo(ms);
+                imagenActual = ms.ToArray();
+                pbImage.Source = new Bitmap(ms);
+            }
+        }
     }
 }
